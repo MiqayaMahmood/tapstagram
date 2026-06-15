83a937d5606b3b92a873f0e28d31db4c38ecbc5d
MiqayaMahmood <miqaya.mahmood@gmail.com>
2026-06-15 20:37:24 +0200

    Delete apps/api/src/plugins/cookies_old.ts
    deleted

--- apps/api/src/plugins/cookies_old.ts
+++
@@ -1,9 +0,0 @@
-﻿import fp from "fastify-plugin";
-import fastifyCookie from "@fastify/cookie";
-
-export default fp(async (app) => {
-    app.register(fastifyCookie, {
-        // optional but recommended if you set signed cookies
-        secret: process.env.COOKIE_SECRET,
-    });
-});
